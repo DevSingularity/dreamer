e3be1bdd9df5ebff9b7c1b06f35b630813661362
Saman Pandey <singularity.1@hotmail.com>
2026-06-19 16:29:44 +0530

    feat: reverse-proxy setup for cloud deployments serving

--- apps/reverse-proxy/index.js
+++ apps/reverse-proxy/index.js
@@ -1,26 +1,28 @@
 const express = require('express')
 const httpProxy = require('http-proxy')
-
 const app = express()
-const PORT = 8000
 
-const BASE_PATH = 'https://dreamer-outputs.s3.ap-south-1.amazonaws.com/__outputs/'
+const PORT = process.env.PORT || 8000
+const BASE_PATH = 'https://dreamer-outputs.s3.ap-south-1.amazonaws.com/__outputs'
+const BASE_DOMAIN = process.env.BASE_DOMAIN || 'singularitydev.xyz'
 
 const proxy = httpProxy.createProxy()
 
+// CURRENT: projectSlug -> subdomain -> s3 bucket path
+// FUTURE TODO: projectSlug -> subdomain -> deployment record in DB -> latest deploymentId -> s3 bucket path
 app.use((req, res) => {
-    const hostname = req.hostname;
-    const subdomain = hostname.split('.')[0];
-
-    // Custom Domain - DB Query
+    const hostname = req.hostname // e.g. "myapp.singularitydev.xyz"
+    const subdomain = hostname.split('.')[0] // "myapp"
+    // TODO: replace with real DB lookup mapping subdomain -> deployment/project slug
     const resolvesTo = `${BASE_PATH}/${subdomain}`
     return proxy.web(req, res, { target: resolvesTo, changeOrigin: true })
 })
 
-proxy.on('proxyReq', (proxyReq, req, res) => {
-    const url = req.url;
-    if (url === '/')
+proxy.on('proxyReq', (proxyReq, req, _res) => {
+    const url = req.url
+    if (url === '/') {
         proxyReq.path += 'index.html'
+    }
 })
 
-app.listen(PORT, () => console.log(`Reverse Proxy Server Running on ${PORT}`))
+app.listen(PORT, () => console.log(`Reverse Proxy Server running on port ${PORT}`))
