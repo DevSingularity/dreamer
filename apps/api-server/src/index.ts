9842aa61314666adcf8de2729edbc508dad20b93
Saman Pandey <singularity.1@hotmail.com>
2026-06-23 11:24:33 +0530

    api(srp): index.ts how does only booting, redis service is now moved to /realtime

--- apps/api-server/src/index.ts
+++ apps/api-server/src/index.ts
@@ -1,30 +1,8 @@
 import { app } from './app';
 import { env } from './lib/env';
-import Redis from 'ioredis';
-import { Server, Socket } from 'socket.io';
+import { startRealtimeGateway } from './realtime';
 
-const subscriber = new Redis(env.REDIS_URL);
-const io = new Server({ cors: { origin: env.FRONTEND_URL } });
-
-io.on('connection', (socket: Socket) => {
-  socket.on('subscribe', (channel: string) => {
-    socket.join(channel);
-    socket.emit('message', `Joined ${channel}`);
-  });
-});
-
-io.listen(9002);
-console.log('Socket Server 9002');
-
-async function initRedisSubscribe(): Promise<void> {
-  console.log('Subscribed to logs....');
-  await subscriber.psubscribe('logs:*');
-  subscriber.on('pmessage', (pattern: string, channel: string, message: string) => {
-    io.to(channel).emit('message', message);
-  });
-}
-
-initRedisSubscribe();
+startRealtimeGateway();
 
 app.listen(env.PORT, () => {
   console.log(`API server is running on port ${env.PORT}`);
